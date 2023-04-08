71df99734e383058762d7711cb4c5d91266b79f7
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-08 09:57:59 -0300

    refaz function de sorteio e adivinhacao de numeros

--- script.js
+++ script.js
@@ -1,60 +1,76 @@
-function sorteia() {
-  return Math.round(Math.random() * 10);
-}
-
-function sorteiaNumeros(quantidade) {
-  var segredos = [];
-
-  var numero = 1;
-
-  while (numero <= quantidade) {
-    var numeroAleatorio = sorteia();
-    var achou = false;
-
-    if (numeroAleatorio !== 0) {
-      for (var posicao = 0; posicao < segredos.length; posicao++) {
-        if (segredos[posicao] == numeroAleatorio) {
-          achou = true;
-          break;
-        }
-      }
-
-      if (achou == false) {
-        segredos.push(numeroAleatorio);
-        numero++;
-      }
-    }
-  }
-
-  return segredos;
-}
-
-var segredos = sorteiaNumeros(3);
-
-console.log(segredos);
-
 var input = document.querySelector('input');
+var button = document.querySelector('button');
+let numeroSorteado = Math.round(Math.random() * 10);
+console.log(numeroSorteado);
 input.focus();
 
+// function sorteia() {
+//   return Math.round(Math.random() * 10);
+// }
+
+// function sorteiaNumeros(quantidade) {
+//   var segredos = [];
+
+//   var numero = 1;
+
+//   while (numero <= quantidade) {
+//     var numeroAleatorio = sorteia();
+//     var achou = false;
+
+//     if (numeroAleatorio !== 0) {
+//       for (var posicao = 0; posicao < segredos.length; posicao++) {
+//         if (segredos[posicao] == numeroAleatorio) {
+//           achou = true;
+//           break;
+//         }
+//       }
+
+//       if (achou == false) {
+//         segredos.push(numeroAleatorio);
+//         numero++;
+//       }
+//     }
+//   }
+
+//   return segredos;
+// }
+
+// var segredos = sorteiaNumeros(3);
+
+// console.log(segredos);
+
+// input.focus();
+
+// function verifica() {
+//   var achou = false;
+
+//   for (var posicao = 0; posicao < segredos.length; posicao++) {
+//     if (input.value == segredos[posicao]) {
+//       alert('Você ACERTOU!');
+//       achou = true;
+//       break;
+//     }
+//   }
+
+//   if (achou == false) {
+//     alert('Você ERROU!');
+//   }
+
+//   input.value = '';
+//   input.focus();
+// }
+
+// function sorteia() {
+//   let numeroSorteado = Math.round(Math.random() * 10);
+// }
+
 function verifica() {
-  var achou = false;
-
-  for (var posicao = 0; posicao < segredos.length; posicao++) {
-    if (input.value == segredos[posicao]) {
-      alert('Você ACERTOU!');
-      achou = true;
-      break;
-    }
+  if (input.value != numeroSorteado) {
+    alert('voce errou');
+  } else {
+    alert('voce acertou');
   }
-
-  if (achou == false) {
-    alert('Você ERROU!');
-  }
-
   input.value = '';
   input.focus();
 }
-
-var button = document.querySelector('button');
-
 button.onclick = verifica;
